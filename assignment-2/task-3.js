43ff10c75212ff4a2626a70b6628b5a32ef62834
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2023-12-28 14:40:56 +0200

    js

---
+++ assignment-2/task-3.js
@@ -0,0 +1,6 @@
+let make = "`I'm In";
+let love = "love";
+let use = "with";
+let deer ="javascript";
+console.log(make +"\n"+"\\"+"\\"+"\n"+love+" "+"\\"+"\\"+" "+'"""'+"'''"+"\n"+"++"+" "+use+" "
++"++"+"\n"+"\\"+'"""'+"\\"+'"""'+"\n"+'""'+deer+'""'+"``" );
